af6b935952e634e62a2738f7af6ed9b7c9a99d0a
YDZ <ydz627@gmail.com>
2017-04-02 19:29:50 +0800

    迁移原来博客里面Xcode8Autolayout新特性的例子

---
+++ Xcode8AutolayoutDemo-master/NewAutolayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewAutolayout
+//
+//  Created by YDZ on 16/7/15.
+//  Copyright © 2016年 YDZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Xcode8AutolayoutDemo-master/NewAutolayout/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NewAutolayout
+//
+//  Created by YDZ on 16/7/15.
+//  Copyright © 2016年 YDZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
