05be6831142e53abdd395b3d3b9c67d024ab2e14
YDZ <ydz627@gmail.com>
2017-04-02 19:33:48 +0800

    增加pod

--- AutoLayoutProfiling-master/AutoLayoutProfiling/ALPRootViewController.m
+++ AutoLayoutProfiling-master/AutoLayoutProfiling/ALPRootViewController.m
@@ -6,18 +6,36 @@
 #import "ALPRootViewController.h"
 #import "ALPAutoLayoutView.h"
 #import "ALPNonAutoLayoutView.h"
+#import <UIView+Yoga.h>
+#import "YGLayout.h"
 
 @implementation ALPRootViewController {
     NSUInteger viewCount;
     UILabel* label;
     UIView* container;
     UITextField* textField;
+    NSMutableDictionary *_resultDictionary;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self setupContainer];
     [self addControls];
+    
+    UIView *view = [[UIView alloc]init];
+    view.backgroundColor = [UIColor redColor];
+    
+    [view configureLayoutWithBlock:^(YGLayout * _Nonnull layout) {
+        layout.isEnabled = YES;
+        layout.width = 100;
+        layout.height = 200;
+        layout.marginLeft = 200;
+        layout.marginTop = 200;
+    }];
+    [self.view addSubview:view];
+    
+    [view.yoga applyLayoutPreservingOrigin:YES];
+    
 }
 
 - (void)setupContainer {
@@ -29,62 +47,112 @@
     [container.subviews makeObjectsPerformSelector:@selector(removeFromSuperview)];
 }
 
+- (void)printfResult {
+    NSLog(@"_resultDictionary = %@",_resultDictionary);
+}
 - (void)addControls {
     UIButton* manualIndependentButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    manualIndependentButton.frame = CGRectMake(0, 0, 200, 30);
-    [manualIndependentButton setTitle:@"Manual Independent" forState:UIControlStateNormal];
+    manualIndependentButton.frame = CGRectMake(0, 50, 300, 30);
+    [manualIndependentButton setTitle:@"用frame方式创建相互无关联的View" forState:UIControlStateNormal];
     [manualIndependentButton addTarget:self action:@selector(addManualIndependent) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:manualIndependentButton];
 
     UIButton* manualNestedButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    manualNestedButton.frame = CGRectMake(0, 30, 200, 30);
-    [manualNestedButton setTitle:@"Manual Nested" forState:UIControlStateNormal];
+    manualNestedButton.frame = CGRectMake(0, 80, 300, 30);
+    [manualNestedButton setTitle:@"用frame方式创建嵌套的View" forState:UIControlStateNormal];
     [manualNestedButton addTarget:self action:@selector(addManualNested) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:manualNestedButton];
 
     UIButton* autoLayoutIndependentButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    autoLayoutIndependentButton.frame = CGRectMake(0, 60, 200, 30);
-    [autoLayoutIndependentButton setTitle:@"AutoLayout Independent" forState:UIControlStateNormal];
+    autoLayoutIndependentButton.frame = CGRectMake(0, 110, 300, 30);
+    [autoLayoutIndependentButton setTitle:@"用AutoLayout方式创建相互无关联的View" forState:UIControlStateNormal];
     [autoLayoutIndependentButton addTarget:self action:@selector(addAutoLayoutIndependent) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:autoLayoutIndependentButton];
 
     UIButton* autoLayoutChainedButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    autoLayoutChainedButton.frame = CGRectMake(0, 90, 200, 30);
-    [autoLayoutChainedButton setTitle:@"AutoLayout Chained" forState:UIControlStateNormal];
+    autoLayoutChainedButton.frame = CGRectMake(0, 140, 300, 30);
+    [autoLayoutChainedButton setTitle:@"用AutoLayout方式创建链式关联的View" forState:UIControlStateNormal];
     [autoLayoutChainedButton addTarget:self action:@selector(addAutoLayoutChained) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:autoLayoutChainedButton];
 
     UIButton* autoLayoutNestedButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    autoLayoutNestedButton.frame = CGRectMake(0, 120, 200, 30);
-    [autoLayoutNestedButton setTitle:@"AutoLayout Nested" forState:UIControlStateNormal];
+    autoLayoutNestedButton.frame = CGRectMake(0, 170, 300, 30);
+    [autoLayoutNestedButton setTitle:@"用AutoLayout方式创建嵌套的View" forState:UIControlStateNormal];
     [autoLayoutNestedButton addTarget:self action:@selector(addAutoLayoutNested) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:autoLayoutNestedButton];
 
     UIButton* clearButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    clearButton.frame = CGRectMake(0, 150, 200, 30);
+    clearButton.frame = CGRectMake(300, 50, 200, 30);
     [clearButton setTitle:@"Clear" forState:UIControlStateNormal];
     [clearButton addTarget:self action:@selector(clearViews) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:clearButton];
+    
+    UIButton* printfButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    printfButton.frame = CGRectMake(300, 80, 200, 30);
+    [printfButton setTitle:@"打印结果" forState:UIControlStateNormal];
+    [printfButton addTarget:self action:@selector(printfResult) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:printfButton];
+    
 
-    label = [[UILabel alloc] initWithFrame:CGRectMake(300, 10, 170, 30)];
+    label = [[UILabel alloc] initWithFrame:CGRectMake(250, 20, 470, 30)];
     label.text = @"Number of views:";
     [self.view addSubview:label];
 
-    textField = [[UITextField alloc] initWithFrame:CGRectMake(490, 0, 100, 40)];
+    textField = [[UITextField alloc] initWithFrame:CGRectMake(490, 50, 200, 40)];
     textField.borderStyle = UITextBorderStyleRoundedRect;
+    textField.placeholder = @"输入要创建View的个数";
     [self.view addSubview:textField];
+    
+    
+    _resultDictionary = [[NSMutableDictionary alloc] init];
+    [_resultDictionary setObject:[[NSMutableDictionary alloc] init] forKey:@"AutoLayout"];
+    [_resultDictionary setObject:[[NSMutableDictionary alloc] init] forKey:@"NestedAutoLayout"];
+    [_resultDictionary setObject:[[NSMutableDictionary alloc] init] forKey:@"ChainedAutoLayout"];
+    [_resultDictionary setObject:[[NSMutableDictionary alloc] init] forKey:@"Frame"];
+    [_resultDictionary setObject:[[NSMutableDictionary alloc] init] forKey:@"NestedFrame"];
+    [_resultDictionary setObject:[[NSMutableDictionary alloc] init] forKey:@"Weex"];
+    [_resultDictionary setObject:[[NSMutableDictionary alloc] init] forKey:@"NestedWeex"];
+    [_resultDictionary setObject:[[NSMutableDictionary alloc] init] forKey:@"Yoga"];
+    [_resultDictionary setObject:[[NSMutableDictionary alloc] init] forKey:@"NestedYoga"];
 }
 
 - (void)addAutoLayoutIndependent {
+    
+    [self clearViews];
+    
+    NSTimeInterval startTime = [NSDate timeIntervalSinceReferenceDate];
+    
     [self addAutoLayoutView:(ALPLayoutTypeIndependent)];
+    
+    NSTimeInterval endTime = [NSDate timeIntervalSinceReferenceDate];
+    
+    [self calculateTimeWithStartTime:startTime endTime:endTime resultName:@"AutoLayout"];
 }
 
 - (void)addAutoLayoutChained {
+    
+    [self clearViews];
+    
+    NSTimeInterval startTime = [NSDate timeIntervalSinceReferenceDate];
+    
     [self addAutoLayoutView:(ALPLayoutTypeChained)];
+    
+    NSTimeInterval endTime = [NSDate timeIntervalSinceReferenceDate];
+    
+    [self calculateTimeWithStartTime:startTime endTime:endTime resultName:@"ChainedAutoLayout"];
 }
 
 - (void)addAutoLayoutNested {
+    
+    [self clearViews];
+    
+    NSTimeInterval startTime = [NSDate timeIntervalSinceReferenceDate];
+    
     [self addAutoLayoutView:(ALPLayoutTypeNested)];
+    
+    NSTimeInterval endTime = [NSDate timeIntervalSinceReferenceDate];
+    
+    [self calculateTimeWithStartTime:startTime endTime:endTime resultName:@"NestedAutoLayout"];
 }
 
 
@@ -96,18 +164,57 @@
 }
 
 - (void)addManualIndependent {
-    [self addNonAutoLayoutView:(ALPLayoutTypeIndependent)];
+    
+    for (int i = 0; i < 1000; i ++) {
+        
+        [self clearViews];
+        
+        NSTimeInterval startTime = [NSDate timeIntervalSinceReferenceDate];
+        
+        [self addNonAutoLayoutView:(ALPLayoutTypeIndependent)];
+        
+        NSTimeInterval endTime = [NSDate timeIntervalSinceReferenceDate];
+        
+        [self calculateTimeWithStartTime:startTime endTime:endTime resultName:@"Frame"];
+    }
+    
 }
 
 - (void)addManualNested {
+    
+    [self clearViews];
+    
+    NSTimeInterval startTime = [NSDate timeIntervalSinceReferenceDate];
+    
     [self addNonAutoLayoutView:(ALPLayoutTypeNested)];
+    
+    NSTimeInterval endTime = [NSDate timeIntervalSinceReferenceDate];
+    
+    [self calculateTimeWithStartTime:startTime endTime:endTime resultName:@"NestedFrame"];
 }
 
 - (void)addNonAutoLayoutView:(ALPLayoutType)type {
+    
     CGSize size = self.view.bounds.size;
     viewCount = (NSUInteger) [textField.text intValue];
     ALPNonAutoLayoutView* nonAutoLayoutView = [[ALPNonAutoLayoutView alloc] initWithFrame:CGRectMake(0, 0, size.width, size.height) type:type viewCount:viewCount];
     [container addSubview:nonAutoLayoutView];
+}
+
+- (void)calculateTimeWithStartTime:(NSTimeInterval)startTime endTime:(NSTimeInterval)endTime resultName:(NSString *)resultName {
+    
+    NSTimeInterval timeInterval = endTime - startTime;
+    
+    NSMutableDictionary *autoLayoutDictionary = _resultDictionary[resultName];
+    NSMutableDictionary *currentTimesDictionary = autoLayoutDictionary[@(viewCount)] ?: [[NSMutableDictionary alloc] init];
+    NSNumber *times = currentTimesDictionary[@"times"] ? : @0;
+    NSNumber *avgTime = currentTimesDictionary[@"avgTime"] ? : @0;
+    currentTimesDictionary[@"avgTime"] = @((times.integerValue * avgTime.doubleValue + timeInterval) / (double)(times.integerValue + 1));
+    currentTimesDictionary[@"times"] = @(times.integerValue + 1);
+    [autoLayoutDictionary setObject:currentTimesDictionary forKey:@(viewCount)];
+    
+    label.text = [NSString stringWithFormat:@"Number of views: %ld | Time: %f | avgTime: %f", (long)viewCount, endTime-startTime,[currentTimesDictionary[@"avgTime"] floatValue]];
+    
 }
 
 
--- AutoLayoutProfiling-master/README.md
+++ AutoLayoutProfiling-master/README.md
@@ -1,3 +1,3 @@
 # Layout Profiling
 
-这是一个测试Auto Layout，Frame，FlexBox布局性能的例子。
+这是一个测试Autolayout，Frame，FlexBox 布局性能的例子
