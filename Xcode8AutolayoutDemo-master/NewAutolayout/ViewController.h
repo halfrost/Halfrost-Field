0b05f0d5a31fdd53ca7357f4fae0c77271c0db87
YDZ <ydz627@gmail.com>
2017-04-02 19:29:50 +0800

    迁移原来博客里面Xcode8Autolayout新特性的例子

---
+++ Xcode8AutolayoutDemo-master/NewAutolayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewAutolayout
+//
+//  Created by YDZ on 16/7/15.
+//  Copyright © 2016年 YDZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Xcode8AutolayoutDemo-master/NewAutolayout/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  NewAutolayout
+//
+//  Created by YDZ on 16/7/15.
+//  Copyright © 2016年 YDZ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
